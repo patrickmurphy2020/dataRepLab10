949cc330db156794c4c79aa068043bb2222000cc
patrickmurphy2020 <g00379725@gmit.ie>
2021-11-29 13:52:33 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,14 +4,19 @@ const port = 4000
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-app.use(cors());
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));//directs it where to go
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
+
+// app.use(cors());
+// app.use(function(req, res, next) {
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+// res.header("Access-Control-Allow-Headers",
+// "Origin, X-Requested-With, Content-Type, Accept");
+// next();
+// });
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -97,7 +102,10 @@ app.get('/api/movies', (req, res) => {
       
 })
 
-
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../build/index.html'));//send the info to index.html so the front page can use all the components
+    });
+    
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
